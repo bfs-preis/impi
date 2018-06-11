836a253280c442558f5b35f6a526762d3e9d38a1
adrian baehler <adrian.baehler@softrain.ch>
2018-06-11 12:52:16 +0200

    release script changes

--- scripts/install&build.js
+++ scripts/install&build.js
@@ -4,7 +4,7 @@ let execSyncOption = { stdio: [0, 1, 2] };
 
 console.log("Current Folder:" + process.cwd());
 child_process.execSync("yarn install", execSyncOption);
-child_process.execSync("./node_modules/.bin/projecto install", execSyncOption);
+child_process.execSync("npx projecto install", execSyncOption);
 
 process.chdir('./electron/app');
 child_process.execSync("yarn install --production", execSyncOption);
